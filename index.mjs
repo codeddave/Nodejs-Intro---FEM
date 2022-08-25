4d9643349b966f0c41f244485fee614d4920b7ff
codeddave <drizzzydave@gmail.com>
2022-08-25 12:51:21 +0100

    updates

--- index.mjs
+++ index.mjs
@@ -1,2 +1,8 @@
 import fs from "fs/promises";
-console.log(fs);
+/* 
+commonjs module implementation, you don't get access to __dirname or filename if not using commonjs
+import path from "path"
+const template1 =  await fs.readFile(path.join(__dirname, "template.html")) */
+
+const template = await fs.readFile(new URL("template.html", import.meta.url));
+//Generates a buffer, can be converted to a string with a .toString method
