4effda424cce180d32e041548cc6be05910ec23f
codeddave <drizzzydave@gmail.com>
2022-08-29 19:20:40 +0100

    fix

--- reddit-cli/reddit.mjs
+++ reddit-cli/reddit.mjs
@@ -10,7 +10,7 @@ const res = await fetch("https://www.reddit.com/.json");
 
 const data = await res.json();
 const children = data?.data?.children;
-const randomPost = children[Math.floor(Math.random() * children.length)];
+const randomPost = children[Math.floor(Math.random() * children?.length)];
 
 const link = `https://www.reddit.com${randomPost.data.permalink}`;
 if (argv.print) {
@@ -21,3 +21,12 @@ if (argv.print) {
 } else {
   open(link);
 }
+
+/* const test = {
+  u: {
+    l: { course: "node.js", a: { hello: "djkahcvjk" } },
+  },
+};
+
+console.log(JSON.stringify(test, null, 2));
+ */
