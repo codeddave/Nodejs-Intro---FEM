57e7323bd698946f3c856cd994dbe30118db436b
codeddave <drizzzydave@gmail.com>
2022-08-29 16:02:10 +0100

    spinning up bare server with the http module

--- server/index.mjs
+++ server/index.mjs
@@ -2,5 +2,27 @@ import http from "http";
 
 const host = "localhost";
 const port = 8000;
+const todo = [];
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("close", () => {
+      console.log(body);
+    });
+
+    res.writeHead(201);
+    res.end("ok");
+  } else {
+    res.writeHead(200);
+    res.end("hi");
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Server started on ${host}:${port}`);
+});
